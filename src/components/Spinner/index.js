21ffdb40ef69080ca2593a3626df6123ea6fde04
kdfwow <kdfwow0422@gmail.com>
2019-09-29 02:34:53 +0300

    logo changeas

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { RiseLoader } from 'react-spinners';
 import classNames from 'classnames';
-import logo from './logo.jpg';
+import logo from './logo.png';
 import './style.css';
 
 export default class Spinner extends React.Component {
